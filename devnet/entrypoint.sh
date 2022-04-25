12334f6e6093a27dae05181cb6a401d59f8ab8ec
Mo <mo.husseini@crypto.com>
2022-04-25 14:31:32 -0400

    run devnet on 0.0.0.0

--- devnet/entrypoint.sh
+++ devnet/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 NODE_OPTIONS="--max-old-space-size=4096" ganache \
-                                        --fork.url https://evm-t3.cronos.org \
+                                        --fork.url https://evm-t3.cronos.org -h 0.0.0.0 \
                                         --fork.blockNumber 2428843 --chain.chainId 338 --chain.networkId 338 \
                                         --gasPrice 5000000000000 -b 6 \
                                         --secure --unlock "0x7ed8cEDbd9c07F66b60ED846e2b710d40A44bC00"
